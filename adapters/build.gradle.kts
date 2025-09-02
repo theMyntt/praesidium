32c2274b58f37c16b6ac46584c7489b356870e45
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-01 22:38:36 -0300

    ci: lombok dependencies

--- adapters/build.gradle.kts
+++ adapters/build.gradle.kts
@@ -18,7 +18,10 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
     implementation("software.amazon.awssdk:dynamodb:2.21.0")
     implementation("software.amazon.awssdk:auth:2.21.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
