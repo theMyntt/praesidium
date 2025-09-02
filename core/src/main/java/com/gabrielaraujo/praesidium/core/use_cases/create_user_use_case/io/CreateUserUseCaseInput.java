7b0afec1c74cc27a415ff251e2f6e9c54bffd628
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-02 14:11:16 -0300

    chore: decorators validations

--- core/src/main/java/com/gabrielaraujo/praesidium/core/use_cases/create_user_use_case/io/CreateUserUseCaseInput.java
+++ core/src/main/java/com/gabrielaraujo/praesidium/core/use_cases/create_user_use_case/io/CreateUserUseCaseInput.java
@@ -1,5 +1,9 @@
 package com.gabrielaraujo.praesidium.core.use_cases.create_user_use_case.io;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,8 +12,20 @@ import java.util.List;
 @Getter
 @Setter
 public class CreateUserUseCaseInput {
+    @NotNull
+    @NotBlank
     private String name;
+
+    @Email
+    @NotNull
+    @NotBlank
     private String email;
+
+    @NotNull
+    @NotBlank
+    @Min(12)
     private String password;
+
+    @NotNull
     private List<String> roles;
 }
