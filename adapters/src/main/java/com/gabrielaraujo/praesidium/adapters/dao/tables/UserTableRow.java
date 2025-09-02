9b99290764d169af79c5c6cadac5cfec4b9624a4
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-01 23:21:14 -0300

    feat: user table row

---
+++ adapters/src/main/java/com/gabrielaraujo/praesidium/adapters/dao/tables/UserTableRow.java
@@ -0,0 +1,27 @@
+package com.gabrielaraujo.praesidium.adapters.dao.tables;
+
+import com.gabrielaraujo.praesidium.adapters.dao.resolvers.DynamoDbTableName;
+import lombok.Builder;
+import lombok.Getter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+import java.util.List;
+import java.util.UUID;
+
+@Builder
+@Getter
+@DynamoDbBean
+@DynamoDbTableName(name = "users")
+public class UserTableRow {
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private List<String> roles;
+
+    @DynamoDbPartitionKey
+    public UUID getId() {
+        return this.id;
+    }
+}
