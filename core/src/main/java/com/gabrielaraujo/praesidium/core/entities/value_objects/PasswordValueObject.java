41c826b13ffb7921e3f29453e36419bce3a6588f
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2025-09-02 20:49:27 -0300

    refactor: using jbcrypt instead of spring-security

--- core/src/main/java/com/gabrielaraujo/praesidium/core/entities/value_objects/PasswordValueObject.java
+++ core/src/main/java/com/gabrielaraujo/praesidium/core/entities/value_objects/PasswordValueObject.java
@@ -1,35 +1,39 @@
 package com.gabrielaraujo.praesidium.core.entities.value_objects;
 
 import lombok.Getter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.mindrot.jbcrypt.BCrypt;
 
 @Getter
 public class PasswordValueObject {
     private final String value;
-
-    private final BCryptPasswordEncoder ENCODER = new BCryptPasswordEncoder(24);
 
     public PasswordValueObject(String value) {
         this(value, true);
     }
 
     public PasswordValueObject(String value, Boolean isToEncrypt) {
+        if (value == null || value.isEmpty()) {
+            throw new IllegalArgumentException("Password cant be blank");
+        }
+        value = value.trim();
+
         if (!isToEncrypt) {
             this.value = value;
             return;
         }
 
-        if (value == null || value.isEmpty()) throw new IllegalArgumentException("Password cant be blank");
-        value = value.trim();
-
-        this.value = ENCODER.encode(value);
+        this.value = BCrypt.hashpw(value, BCrypt.gensalt(12));
     }
 
     public Boolean compare(String rawPassword) {
-        if (rawPassword == null || rawPassword.isEmpty()) throw new IllegalArgumentException("Plain Password cant be blank");
-        if (this.value == null || value.isEmpty()) throw new IllegalArgumentException("Password cant be blank");
+        if (rawPassword == null || rawPassword.isEmpty()) {
+            throw new IllegalArgumentException("Plain Password cant be blank");
+        }
+        if (this.value == null || value.isEmpty()) {
+            throw new IllegalArgumentException("Password cant be blank");
+        }
 
-        return ENCODER.matches(rawPassword, this.value);
+        return BCrypt.checkpw(rawPassword, this.value);
     }
 
 }
